88b1bea045c45677d396cdb0549882fc5cee3815
Lukas <lukasmilten@gmail.com>
2019-08-09 18:43:50 +0300

    Tests to check basic item value adjustment

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -7,12 +7,23 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void CheckUpdatedItemName()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            // Assert that they have the same name:
+            Assert.AreEqual("+5 Dexterity Vest", Items[0].Name);
+        }
+
+        [Test]
+        public void CheckUpdatedItemValues() {
+            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            // Assert that the SellIn and Quality values were adjusted correctly:
+            Assert.AreEqual(9, Items[0].SellIn);
+            Assert.AreEqual(19, Items[0].Quality);
         }
     }
 }
