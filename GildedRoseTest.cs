26cf2d100c963edcf7ffecbb8e6271d74bb4bb26
Lukas <lukasmilten@gmail.com>
2019-08-10 02:40:54 +0300

    Tests for quality factor increase

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -24,6 +24,35 @@ namespace csharp {
         }
 
         [Test]
+        public void QualityFactorIncreaseTest() {
+            /*
+             * Some items increase in quality depending on their SellIn value:
+             * - Aged Brie increases by 1 when its SellIn value is >= 0,
+             *   - or its quality increases by 2 when the SellIn value is < 0.
+             * - Backstage passes increase in quality by 1 when their SellIn value is > 10,
+             *   - or their quality increases by 2 when 10 >= SellIn < 5,
+             *   - or the quality increases by 3 when 5 >= SellIn < 0,
+             *   - when the SellIn value is 0 the quality is set to 0.
+             */
+
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 0 },
+                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 },
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 0},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 0},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 0},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20}};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(1, Items[0].Quality, "Aged Brie' quality should increase by 1 when SellIn >= 0");
+            Assert.AreEqual(2, Items[1].Quality, "Aged Brie' quality should increase by 2 when SellIn < 0");
+            Assert.AreEqual(1, Items[2].Quality, "Backstage passes' quality should increase by 1 when SellIn > 10");
+            Assert.AreEqual(2, Items[3].Quality, "Backstage passes' quality should increase by 2 when 10 >= SellIn < 5");
+            Assert.AreEqual(3, Items[4].Quality, "Backstage passes' quality should increase by 3 when 5 >= SellIn < 0");
+            Assert.AreEqual(0, Items[5].Quality, "Backstage passes' quality should be 0 when SellIn < 0");
+        }
+
+        [Test]
         public void UpdateAgedBrieValuesTest() {
             IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 } };
             GildedRose app = new GildedRose(Items);
@@ -40,7 +69,7 @@ namespace csharp {
         }
 
         [Test]
-        public void QualityCantBeLowerThanZeroTest() {
+        public void QualityFactorNotLessThanZeroTest() {
             IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
 
@@ -50,24 +79,37 @@ namespace csharp {
         }
 
         [Test]
-        public void QualityCantBeHigherThanFiftyTest() {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 } };
-            GildedRose app = new GildedRose(Items);
+        public void QualityFactorNotHigherThanFiftyTest() {
+            /* 
+             * Test so the Quality value does not increase over 50 for items which increase it.
+             */
 
-            // Assert that the Quality value does not go higher than 50:
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 },
+                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 },
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 50},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 48}};
+            GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual(50, Items[0].Quality, "False Quality value, expected 50");
+
+            Assert.AreEqual(50, Items[0].Quality, "Aged Brie' quality should not increase over 50");
+            Assert.AreEqual(50, Items[1].Quality, "Aged Brie' quality should not increase over 50");
+            Assert.AreEqual(50, Items[2].Quality, "Backstage passes' quality should not increase over 50");
+            Assert.AreEqual(50, Items[3].Quality, "Backstage passes' quality should not increase over 50");
+            Assert.AreEqual(50, Items[4].Quality, "Backstage passes' quality should not increase over 50");
         }
 
         [Test]
         public void ConjuredItemsDegradeTwiceAsFastTest() {
-            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 10, Quality = 20 },
-                                                 new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 20 }};
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 10, Quality = 2 },
+                                                 new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 4 }};
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
-            Assert.AreEqual(18, Items[0].Quality, "False Quality value, expected 18");
-            Assert.AreEqual(16, Items[1].Quality, "False Quality value, expected 16");
+            // Conjured items should decrease in quality by 2 before the sell by date:
+            Assert.AreEqual(0, Items[0].Quality, "False Quality value, expected 0");
+            // Conjured items should decrease in quality by 4 after the sell by date:
+            Assert.AreEqual(0, Items[1].Quality, "False Quality value, expected 0");
         }
 
         //[Test]
