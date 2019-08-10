355067179d56e8164b74c2d12180f9a4df44bb37
Lukas <lukasmilten@gmail.com>
2019-08-10 04:26:53 +0300

    Code refactoring and more tests

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -4,23 +4,100 @@ using System.Collections.Generic;
 namespace csharp {
     [TestFixture]
     public class GildedRoseTest {
+
         [Test]
-        public void UpdateItemNameTest() {
-            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            // Assert that they have the same name:
-            Assert.AreEqual("+5 Dexterity Vest", Items[0].Name, "False Name string, expected '+5 Dexterity Vest'");
+        public void ValidateItemCreationTest() {
+            /*
+             * Check if the item is being created successfully.
+             */
+
+            Item item = new Item { Name = "Basic Item", SellIn = 10, Quality = 20 };
+
+            Assert.AreEqual("Basic Item", item.Name);
+            Assert.AreEqual(10, item.SellIn);
+            Assert.AreEqual(20, item.Quality);
         }
 
         [Test]
-        public void UpdateBasicItemValuesTest() {
-            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+        public void ValidateItemNameAfterUpdateTest() {
+            /*
+             * Check if the item name wasn't changed in an unwanted way after being updated.
+             */
+
+            IList<Item> Items = new List<Item> { new Item { Name = "Basic Item", SellIn = 10, Quality = 1 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            // Assert that the SellIn and Quality values were adjusted correctly:
-            Assert.AreEqual(9, Items[0].SellIn, "False SellIn value, expected 9");
-            Assert.AreEqual(19, Items[0].Quality, "False Quality value, expected 19");
+
+            Assert.AreEqual("Basic Item", Items[0].Name, "The item name should not be changed");
+        }
+
+        [Test]
+        public void QualityFactorDecreaseTest() {
+            /*
+             * Normal items decrease in quality by 1 when the SellIn value is >= 0,
+             * - but they decrease in quality by 2 when the SellIn value is < 0,
+             * - the quality should also never be below 0
+             */
+
+            IList<Item> Items = new List<Item> { new Item { Name = "Basic Item", SellIn = 10, Quality = 1 },
+                                                 new Item { Name = "Basic Item", SellIn = 0, Quality = 2 },
+                                                 new Item { Name = "Basic Item", SellIn = 5, Quality = -1 }};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(0, Items[0].Quality, "The Quality should decrease by 1 when SellIn >= 0");
+            Assert.AreEqual(0, Items[1].Quality, "The Quality should decrease by 2 when SellIn < 0");
+            Assert.AreEqual(0, Items[2].Quality, "The Quality should never be below 0");
+        }
+
+        [Test]
+        public void QualityFactorIncreaseTest() {
+            /*
+             * Some items increase in quality depending on their SellIn value:
+             * - Aged Brie increases by 1 when its SellIn value is >= 0,
+             *   - or its quality increases by 2 when the SellIn value is < 0.
+             * - Backstage passes increase in quality by 1 when their SellIn value is > 10,
+             *   - or their quality increases by 2 when 10 >= SellIn < 5,
+             *   - or the quality increases by 3 when 5 >= SellIn < 0,
+             *   - when the SellIn value is 0 the quality is set to 0.
+             */
+
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 0 },
+                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 },
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 0},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 0},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 0},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20}};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(1, Items[0].Quality, "Aged Brie' quality should increase by 1 when SellIn >= 0");
+            Assert.AreEqual(2, Items[1].Quality, "Aged Brie' quality should increase by 2 when SellIn < 0");
+            Assert.AreEqual(1, Items[2].Quality, "Backstage passes' quality should increase by 1 when SellIn > 10");
+            Assert.AreEqual(2, Items[3].Quality, "Backstage passes' quality should increase by 2 when 10 >= SellIn < 5");
+            Assert.AreEqual(3, Items[4].Quality, "Backstage passes' quality should increase by 3 when 5 >= SellIn < 0");
+            Assert.AreEqual(0, Items[5].Quality, "Backstage passes' quality should be 0 when SellIn < 0");
+        }
+
+        [Test]
+        public void QualityFactorNotHigherThanFiftyTest() {
+            /* 
+             * Test so the Quality value does not increase over 50 for items which increase it.
+             */
+
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 },
+                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 },
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 50},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 48}};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(50, Items[0].Quality, "Aged Brie' quality should not increase over 50");
+            Assert.AreEqual(50, Items[1].Quality, "Aged Brie' quality should not increase over 50");
+            Assert.AreEqual(50, Items[2].Quality, "Backstage passes' quality should not increase over 50");
+            Assert.AreEqual(50, Items[3].Quality, "Backstage passes' quality should not increase over 50");
+            Assert.AreEqual(50, Items[4].Quality, "Backstage passes' quality should not increase over 50");
         }
 
         [Test]
@@ -59,92 +136,20 @@ namespace csharp {
         }
 
         [Test]
-        public void QualityFactorIncreaseTest() {
-            /*
-             * Some items increase in quality depending on their SellIn value:
-             * - Aged Brie increases by 1 when its SellIn value is >= 0,
-             *   - or its quality increases by 2 when the SellIn value is < 0.
-             * - Backstage passes increase in quality by 1 when their SellIn value is > 10,
-             *   - or their quality increases by 2 when 10 >= SellIn < 5,
-             *   - or the quality increases by 3 when 5 >= SellIn < 0,
-             *   - when the SellIn value is 0 the quality is set to 0.
-             */
-
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 0 },
-                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 0},
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 0},
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 0},
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20}};
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-
-            Assert.AreEqual(1, Items[0].Quality, "Aged Brie' quality should increase by 1 when SellIn >= 0");
-            Assert.AreEqual(2, Items[1].Quality, "Aged Brie' quality should increase by 2 when SellIn < 0");
-            Assert.AreEqual(1, Items[2].Quality, "Backstage passes' quality should increase by 1 when SellIn > 10");
-            Assert.AreEqual(2, Items[3].Quality, "Backstage passes' quality should increase by 2 when 10 >= SellIn < 5");
-            Assert.AreEqual(3, Items[4].Quality, "Backstage passes' quality should increase by 3 when 5 >= SellIn < 0");
-            Assert.AreEqual(0, Items[5].Quality, "Backstage passes' quality should be 0 when SellIn < 0");
-        }
-
-        [Test]
-        public void UpdateAgedBrieValuesTest() {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 } };
-            GildedRose app = new GildedRose(Items);
-
-            // Assert that the SellIn and Quality values were adjusted correctly:
-            app.UpdateQuality();
-            Assert.AreEqual(-1, Items[0].SellIn, "False SellIn value, expected -1");
-            Assert.AreEqual(12, Items[0].Quality, "False Quality value, expected 12");
-
-            // Assert that the Quality value was increased two folds:
-            app.UpdateQuality();
-            Assert.AreEqual(-2, Items[0].SellIn, "False SellIn value, expected -2");
-            Assert.AreEqual(14, Items[0].Quality, "False Quality value, expected 14");
-        }
-
-        [Test]
-        public void QualityFactorNotLessThanZeroTest() {
-            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-
-            // Assert that the Quality value does not go lower than 0:
-            app.UpdateQuality();
-            Assert.AreEqual(0, Items[0].Quality, "False Quality value, expected 0");
-        }
-
-        [Test]
-        public void QualityFactorNotHigherThanFiftyTest() {
-            /* 
-             * Test so the Quality value does not increase over 50 for items which increase it.
-             */
-
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 },
-                                                 new Item { Name = "Aged Brie", SellIn = 0, Quality = 49 },
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 50},
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
-                                                 new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 48}};
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-
-            Assert.AreEqual(50, Items[0].Quality, "Aged Brie' quality should not increase over 50");
-            Assert.AreEqual(50, Items[1].Quality, "Aged Brie' quality should not increase over 50");
-            Assert.AreEqual(50, Items[2].Quality, "Backstage passes' quality should not increase over 50");
-            Assert.AreEqual(50, Items[3].Quality, "Backstage passes' quality should not increase over 50");
-            Assert.AreEqual(50, Items[4].Quality, "Backstage passes' quality should not increase over 50");
-        }
-
-        [Test]
         public void ConjuredItemsDegradeTwiceAsFastTest() {
+            /*
+             * Conjured items are special because they decrease in quality twice as fast as other items:
+             * - Their quality decreases by 2 when SellIn is >= 0,
+             *  - or their quality decreases by 4 when SellIn is < 0
+             */
+
             IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 10, Quality = 2 },
                                                  new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 4 }};
             GildedRose app = new GildedRose(Items);
 
             app.UpdateQuality();
-            // Conjured items should decrease in quality by 2 before the sell by date:
-            Assert.AreEqual(0, Items[0].Quality, "False Quality value, expected 0");
-            // Conjured items should decrease in quality by 4 after the sell by date:
-            Assert.AreEqual(0, Items[1].Quality, "False Quality value, expected 0");
+            Assert.AreEqual(0, Items[0].Quality, "Conjured item quality should decrease by 2 when SellIn >= 0");
+            Assert.AreEqual(0, Items[1].Quality, "Conjured item quality should decrease by 4 when SellIn < 0");
         }
 
         //[Test]
